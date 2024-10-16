1c0226e75a4f822331fbb0e09b0fecfa8e10cf12
Nafikova_Julia <nafikova91@yandex.ru>
2024-10-16 16:03:16 +0300

    basic task

--- src/product.py
+++ src/product.py
@@ -12,6 +12,12 @@ class Product:
         self.__price = price
         self.quantity = quantity
 
+    def __str__(self):
+        return f'{self.name}, {self.__price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.price * other.quantity
+
     @classmethod
     def new_product(cls, dict_product, products=None):
         if products:
@@ -36,3 +42,13 @@ class Product:
             if check_input != 'y':
                 return
         self.__price = new_price
+
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1 + product2)
+    print(product1 + product3)
+    print(product2 + product3)
