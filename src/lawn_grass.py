71fe22ecd4f315935ec4a24dbeea65773d8b811d
Nafikova_Julia <nafikova91@yandex.ru>
2024-10-25 08:20:30 +0300

    add new tests

--- src/lawn_grass.py
+++ src/lawn_grass.py
@@ -1,5 +1,6 @@
 from src.product import Product
 
+
 class LawnGrass(Product):
     def __init__(self, name, description, price, quantity, country, germination_period, color):
         super().__init__(name, description, price, quantity)
@@ -11,4 +12,3 @@ class LawnGrass(Product):
         if type(other) is LawnGrass:
             return self.price * self.quantity + other.price * other.quantity
         raise TypeError
-
